7a0b3f003b6322f6bed7132af0a331c7bc2326cc
birkses <birkses@users.noreply.github.com>
2017-01-03 14:37:17 +0900

    Update keyboard-startup.sh

--- keyboard-startup.sh
+++ keyboard-startup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #sleep 20 seconds so that the script is initialised after other xfce scripts to avoid race conditions
+# can put code in ~/.bash_profile for it to execute on login
 sleep 20
 #switch ctrl to capslock key and leave caps lock unassigned 
 setxkbmap -layout us -option ctrl:nocaps
