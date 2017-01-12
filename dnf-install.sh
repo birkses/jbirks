8c8359b07a7e321f7d3a0c80853afcbdfccfdbab
Jordan <jordan@localhost.localdomain>
2017-01-12 17:34:06 +1000

    Add update to install script

--- dnf-install.sh
+++ dnf-install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+yes | dnf update
 yes | dnf update vim-common vim-minimal
 yes | dnf install rxvt-unicode-256color irssi irssi-xmpp vim-X11 trash-cli xmonad ghc-xmonad-contrib-devel screenfetch htop surfraw w3m xlockmore qutebrowser figlet feh xautolock
 
