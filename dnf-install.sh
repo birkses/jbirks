492a234cb8635bfeb0d76efeb87adb8ec2117777
Jordan <jordan@localhost.localdomain>
2017-01-12 19:34:13 +1000

    Fixed scripts for gnome install

--- dnf-install.sh
+++ dnf-install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 yes | dnf update
 yes | dnf update vim-common vim-minimal
-yes | dnf install rxvt-unicode-256color irssi irssi-xmpp vim-X11 trash-cli xmonad ghc-xmonad-contrib-devel screenfetch htop surfraw w3m xlockmore qutebrowser figlet feh xautolock
+yes | dnf install rxvt-unicode-256color irssi irssi-xmpp vim-X11 trash-cli xmonad ghc-xmonad-contrib-devel screenfetch htop surfraw w3m  qutebrowser figlet feh 
+# this breaks gnome with xmonad if installed
+# yes | dnf install xlockmore xautolock
 
