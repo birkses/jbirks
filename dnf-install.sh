9940e6e594806e994edb8b88bbdae48db94df622
Jordan <jordan@localhost.localdomain>
2017-01-13 21:52:25 +1000

    Removed irssi use profanity instead

--- dnf-install.sh
+++ dnf-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 yes | dnf update
 yes | dnf update vim-common vim-minimal
-yes | dnf install rxvt-unicode-256color irssi irssi-xmpp vim-X11 trash-cli xmonad ghc-xmonad-contrib-devel screenfetch htop surfraw w3m  qutebrowser figlet feh zsh util-linux-user
+yes | dnf install rxvt-unicode-256color vim-X11 trash-cli xmonad ghc-xmonad-contrib-devel screenfetch htop surfraw w3m  qutebrowser figlet feh zsh util-linux-user
 # this breaks gnome with xmonad if installed
 # yes | dnf install xlockmore xautolock
 
