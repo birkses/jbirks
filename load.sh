4f50956439f6fa266608abb480a9cf9660c31634
Jordan Birks <jbirks@jbirks-pcbarrukka.local>
2018-04-06 14:15:13 +1000

    remove unused config files and remove piped yes command from dnf install script

--- load.sh
+++ load.sh
@@ -5,7 +5,4 @@ cp -i .Xdefaults ~/
 cp -i .xmonad/xmonad.hs ~/.xmonad/xmonad.hs
 cp -i .bash_profile ~/
 cp -i .aliases ~/
-cp -i .zprofile ~/
-cp -i .zshrc ~/
-cp -i .ideavimrc ~/
 git config --global help.autocorrect 5
