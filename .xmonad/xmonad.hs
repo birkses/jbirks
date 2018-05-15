88c43000905599976a48d90e3caa81b7a3edbded
Jordan Birks <jbirks@jbirks-pcbarrukka.local>
2018-05-15 14:26:01 +1000

    Fix xmonad gaps

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -9,10 +9,17 @@
     import XMonad.Util.EZConfig
     import XMonad.Hooks.SetWMName
     import XMonad.Actions.SpawnOn
+    import XMonad.Layout.MouseResizableTile
     import XMonad.Layout.Spacing
+    import XMonad.Layout.Gaps
     import qualified XMonad.StackSet as W
 
     myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
+    myLayout = gaps [(U,10), (D,10), (L,10), (R,10)] $ mouseResizableTile
+           { masterFrac = 1/2
+           , fracIncrement = 0.05
+           , draggerType = FixedDragger 10 10
+           }
 
     main = xmonad $ ewmh gnomeConfig
         { borderWidth        = 3
@@ -20,25 +27,23 @@
         , workspaces         = myWorkspaces
         , manageHook = manageSpawn <+> manageHook def
         , modMask            = mod4Mask
-        , layoutHook  = spacing 6 $ Tall 1 (3/100) (1/2)
+        , layoutHook  = myLayout
         , terminal           = "urxvt"
-        , focusFollowsMouse  = False
         , normalBorderColor  = "#FFDE95"
-        , focusedBorderColor = "#FFDE95"
+        , focusedBorderColor = "#7F7F4A"
+        , focusFollowsMouse  = False
         , startupHook = do
             setWMName "LG3D"
-            spawnOn "1" "urxvt -e glances"
-            spawnOn "1" "urxvt -e  sh -c 'echo \"di\" | bmon -p wlp3s0*'"
+            spawnOn "3" "urxvt -e htop"
+            spawnOn "3" "urxvt -e  sh -c 'echo \"di\" | bmon -p wlp3s0*'"
             spawnOn "2" "bash -c 'http_proxy=http://10.10.10.211:31060 https_proxy=http://10.10.10.211:31060 qutebrowser'"
-            spawnOn "3" "urxvt -e ranger"
+            spawnOn "1" "bash -c 'cd /home/jbirks/work/msm2/governor/src/json_schemas; urxvt -e ranger'"
         } `additionalKeys` ( [
           ((mod4Mask, xK_f), spawn "http_proxy=http://10.10.10.211:31060 https_proxy=http://10.10.10.211:31060 qutebrowser"),
-          ((mod4Mask, xK_g), spawn "google-chrome"),
-          ((mod4Mask .|. shiftMask, xK_l), spawn "xlock -mode grav -geometry 1x1-1-1")
+          ((mod4Mask, xK_g), spawn "google-chrome")
         ]
         ++
         [((m .|. mod4Mask, k), windows $ f i)
              | (i, k) <- zip myWorkspaces [xK_1 .. xK_9]
              , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
         )
-
