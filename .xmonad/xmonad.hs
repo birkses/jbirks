c635025fa960219c8f32bdc33fce576aad15505d
Jordan Birks <jbirks@boeing.com>
2018-06-29 10:34:28 +1000

    fix scoping

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -23,11 +23,12 @@
     myTerminal = "urxvtc"
     myWorkspaces = [ "1","2","3","4","5","6","7","8","9" ]
     myBorder = gaps [ (U,10), (D,10), (L,10), (R,10) ]
-    mainLayout = myBorder $ mouseResizableTile
-        { masterFrac       = 1/2
-        , fracIncrement    = 0.05
-        , draggerType      = FixedDragger 10 10 }
-    myLayout = smartBorders $  mainLayout ||| Dishes 3 (1/6) ||| Grid ||| noBorders Full
+    myLayout = mainLayout ||| Dishes 3 (1/6) ||| Grid ||| noBorders Full
+      where
+        mainLayout = myBorder $ mouseResizableTile
+            { masterFrac       = 1/2
+            , fracIncrement    = 0.05
+            , draggerType      = FixedDragger 10 10 }
     myScratchpads =
         [ (NS "qutebrowser" "http_proxy=http://10.10.10.211:31060 https_proxy=http://10.10.10.211:31060 qutebrowser --basedir /home/jbirks/.config/qutebrowser --qt-arg name scratchqt" (appName =? "scratchqt") defaultFloating)
         , (NS "tty-clock" "urxvtc -e tty-clock" (title =? "tty-clock") defaultFloating) ]
@@ -43,7 +44,7 @@
                              <+> manageHook def
                              <+> scratchpadManageHook (W.RationalRect 0.125 0.125 0.75 0.75)
         , modMask            = mod4Mask
-        , layoutHook         = myLayout
+        , layoutHook         = smartBorders $ myLayout
         , terminal           = myTerminal
         , normalBorderColor  = "#FFDE95"
         , focusedBorderColor = "#FF9900"
