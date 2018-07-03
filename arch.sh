c818287433b0098b06d377011a30a454ea20f29e
birkses <birkses@github.com>
2018-07-03 19:07:09 +1000

    add Xdefaults file

--- arch.sh
+++ arch.sh
@@ -1,6 +1,4 @@
-sudo pacman -S zathura zathura-pdf-mupdf feh ffmpeg mpv pywal xmonad xorg-xrandr terminus-font xmonad-contrib nvidia gvim
-
-# ranger
+sudo pacman -S zathura zathura-pdf-mupdf feh ffmpeg mpv pywal xmonad xorg-xrandr terminus-font xmonad-contrib nvidia gvim ranger
 
 systemctl enable dhcpd
 systemctl start dhcpd
