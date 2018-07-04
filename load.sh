0623a937051c1e4cd6e16c5d0e4703bd2408996a
birkses <birkses@github.com>
2018-07-04 19:07:48 +1000

    clean up unused lines

--- load.sh
+++ load.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 cp -i .bashrc ~/
 cp -i .vimrc ~/
-cp -i .Xdefaults ~/
 cp -i .inputrc ~/
 cp -i .xmonad/xmonad.hs ~/.xmonad/xmonad.hs
 cp -i .bash_profile ~/
 cp -i .aliases ~/
+mkdir -p ~/.config/ && cp -ir .config/* ~/.config/
 git config --global help.autocorrect 5
