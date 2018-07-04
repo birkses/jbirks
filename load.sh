f3cb8c4b3135c7008b9637821e66d4737cafd1e8
birkses <birkses@github.com>
2018-07-04 19:37:10 +1000

    move .profile

--- load.sh
+++ load.sh
@@ -4,5 +4,6 @@ cp -i .vimrc ~/
 cp -i .inputrc ~/
 cp -i .xmonad/xmonad.hs ~/.xmonad/xmonad.hs
 cp -i .bash_profile ~/
+cp -i .profile ~/
 cp -i .aliases ~/
 mkdir -p ~/.config/ && cp -ir .config/* ~/.config/
